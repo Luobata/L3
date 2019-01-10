75aa4f941e64583f3140cde6d31fc102124f0a46
yaochenghao <yaochenghao@bytedance.com>
2019-01-10 22:19:56 +0800

    add: text module

---
+++ src/core/label.ts
@@ -0,0 +1,7 @@
+/**
+ * @desc 标题部分
+ */
+
+import Text from '@/core/text.ts';
+
+export default class Label extends Text {}
---
+++ src/core/text.ts
@@ -0,0 +1,12 @@
+/**
+ * @desc text 文字渲染
+ */
+
+/**
+ * default class
+ */
+export default class Text {
+    public postion: number;
+
+    constructor() {}
+}
