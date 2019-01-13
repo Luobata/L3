9f0ccb1ccfaa018a3cd524fbfef5b0496251b37d
yaochenghao <yaochenghao@bytedance.com>
2019-01-13 16:34:27 +0800

    add: test-case

--- test/test.js
+++ test/test.js
@@ -1,4 +1,21 @@
 import Chart from '../src/';
 // import Chart from '../dist/simply-chart';
 
-window.onload = () => {};
+window.onload = () => {
+    const basicConifg = {
+        width: 200,
+        height: 100,
+        padding: 10,
+        renderType: 'none',
+        renderTime: 200 / 60,
+        framePerSecond: 60,
+    };
+
+    const j = new Chart.pie({
+        base: {
+            ...basicConifg,
+        },
+    });
+
+    j.update([2, 1, 3, 2.5, 1.5]).render();
+};
